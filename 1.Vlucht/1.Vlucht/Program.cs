02afeb91b03e82b2002dd5340a5534a4fcf03798
rbnm <rubenmampaey99@gmail.com>
2018-04-25 17:20:20 +0200

    Initial commit

---
+++ 1.Vlucht/1.Vlucht/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Vlucht
+{
+    class Program
+    {
+       static void Main(string[] args)
+       {
+
+            Vlucht vlucht0 = new Vlucht(Vlucht.Bestemmingen.Duitsland, DateTime.Now, DateTime.Now.AddHours(5)); // 3
+           Vlucht vlucht1 = new Vlucht(Vlucht.Bestemmingen.Duitsland, DateTime.Now, DateTime.Now.AddHours(5)); // 3
+           Vlucht vlucht2 = new Vlucht(Vlucht.Bestemmingen.Duitsland, DateTime.Now, DateTime.Now.AddHours(5)); //3
+
+            //Vlucht vlucht2 = new Vlucht(8);
+            Console.WriteLine(vlucht0);
+           // Console.WriteLine(vlucht1);
+           // Console.WriteLine(vlucht2);
+            //Console.WriteLine(vlucht2);
+        }
+    }
+}
---
+++ 1.Vlucht/1.Vlucht/Vlucht.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Vlucht
+{
+    class Vlucht
+    {
+        public enum Bestemmingen
+        {
+            Frankrijk, Engeland, Duitsland
+        }
+        public static int Vluchtnummer { get; set; }
+        public Bestemmingen Bestemming { get; set; }
+        //public string Vertrek { get; set; }
+       // private string vertrek; mag weg, want er wordt niets mee gedaan
+
+        public string Vertrek
+        {
+            get { return "België"; }
+        }
+
+
+        public DateTime VertrekDatum { get; set; }
+        public DateTime Terugdatum { get; set; }
+
+        public Vlucht(Bestemmingen bestemming, DateTime vertrekDatum, DateTime terugdatum)
+        {
+            Vluchtnummer++;
+            Bestemming = bestemming;
+            VertrekDatum = vertrekDatum;
+            Terugdatum = terugdatum;
+        }
+
+        //public Vlucht(int vluchtnummer) :this(vluchtnummer, Bestemmingen.Duitsland, DateTime.Today, DateTime.Today.AddHours(2))
+        //{
+        //}
+
+        public int Vluchtduur()
+        {
+            int duur = (Terugdatum.Hour - VertrekDatum.Hour);
+            return duur;
+        }
+
+        public override string ToString()
+        {
+            string resultaat = "Vlucht " + Vluchtnummer + " Vliegt van " + Vertrek + " naar " + Bestemming + "\n";
+            resultaat += "En duurt " + Vluchtduur() + " uur.";
+            return resultaat;
+        }
+    }
+}
